0d21d799b9865f9ab3705914c75500fd38222d52
Stiven Ramírez Arango <sramir70@eafit.edu.co>
2019-11-29 14:45:05 -0500

    Delete comments

--- src/router/index.js
+++ src/router/index.js
@@ -34,8 +34,6 @@ const router = new VueRouter({
 router.beforeEach((to, from, next) => {
   const user = auth.currentUser
   if (to.matched.some(record => record.meta.requiresAuth)) {
-    // this route requires auth, check if logged in
-    // if not, redirect to login page.
     if (user) {
       next()
     } else {
@@ -44,7 +42,7 @@ router.beforeEach((to, from, next) => {
       })
     }
   } else {
-    next() // make sure to always call next()!
+    next()
   }
 })
 
